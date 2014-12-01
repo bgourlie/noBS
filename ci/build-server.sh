853f5b0fb957509fb5dac0e174bce3b7e0b2516c
W. Brian Gourlie <bgourlie@gmail.com>
2014-12-01 00:38:48 -0600

    Configurable deploy branch for client/server.

--- ci/build-server.sh
+++ ci/build-server.sh
@@ -24,10 +24,14 @@ cp -rf ../AzureDeployDeps/System.ComponentModel.Annotations ./deploy/approot/pac
 
 # (HACK) Patch generated web.config so that we can serve static json files (version.json)
 pub global run nvp:nvp patchWebConfig -c ./deploy/wwwroot/web.config
- 
-echo "==========================="
-echo "      DEPLOY TO AZURE      "
-echo "==========================="
 
-find ./deploy -type f -exec curl --fail --ftp-create-dirs -T {} -u $AZURE_FTP_USER:$AZURE_FTP_PASSWORD ftp://waws-prod-dm1-003.ftp.azurewebsites.windows.net/site/wwwroot/nobs-api/{} \;
-
+if [ "$TRAVIS_BRANCH" = "$DEPLOY_BRANCH" ];
+then
+	echo "==========================="
+	echo "      DEPLOY TO AZURE      "
+	echo "==========================="
+	find ./deploy -type f -exec curl --fail --ftp-create-dirs -T {} -u $AZURE_FTP_USER:$AZURE_FTP_PASSWORD ftp://waws-prod-dm1-003.ftp.azurewebsites.windows.net/site/wwwroot/nobs-api/{} \;
+else
+	echo "Skipping server deployment because branch is not $DEPLOY_BRANCH."	
+	exit 0
+fi

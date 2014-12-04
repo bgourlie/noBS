d28b85a311950e9b67978e546d3512f7329de0f4
W. Brian Gourlie <bgourlie@gmail.com>
2014-12-03 23:18:40 -0600

    Fix deploy script.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,7 +6,7 @@ then
 	echo "==========================="
 	echo "      DEPLOY TO AZURE      "
 	echo "==========================="
-	find ./deploy -type f -exec curl --fail --ftp-create-dirs -T {} -u $AZURE_FTP_USER:$AZURE_FTP_PASSWORD ftp://waws-prod-dm1-003.ftp.azurewebsites.windows.net/site/wwwroot/nobs-api/{} \;
+	find $TRAVIS_BUILD_DIR/server/src/FitLog.Api/deploy -type f -exec curl --fail --ftp-create-dirs -T {} -u $AZURE_FTP_USER:$AZURE_FTP_PASSWORD ftp://waws-prod-dm1-003.ftp.azurewebsites.windows.net/site/wwwroot/nobs-api/{} \;
 else
 	echo "Skipping server deployment because branch is not $DEPLOY_BRANCH."	
 	exit 0

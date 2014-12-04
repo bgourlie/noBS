acfaf000495e766ccf324957015cdadef24cbfea
W. Brian Gourlie <bgourlie@gmail.com>
2014-12-04 08:25:20 -0600

    Fix deploy location.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,7 +6,8 @@ then
 	echo "==========================="
 	echo "      DEPLOY TO AZURE      "
 	echo "==========================="
-	find $TRAVIS_BUILD_DIR/server/src/FitLog.Api/deploy -type f -exec curl --fail --ftp-create-dirs -T {} -u $AZURE_FTP_USER:$AZURE_FTP_PASSWORD ftp://waws-prod-dm1-003.ftp.azurewebsites.windows.net/site/wwwroot/nobs-api/{} \;
+	cd $TRAVIS_BUILD_DIR/server/src/FitLog.Api
+	find ./deploy -type f -exec curl --fail --ftp-create-dirs -T {} -u $AZURE_FTP_USER:$AZURE_FTP_PASSWORD ftp://waws-prod-dm1-003.ftp.azurewebsites.windows.net/site/wwwroot/nobs-api/{} \;
 else
 	echo "Skipping server deployment because branch is not $DEPLOY_BRANCH."	
 	exit 0

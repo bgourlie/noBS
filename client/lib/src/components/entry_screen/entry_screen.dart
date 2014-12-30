63ae2fb6e61391087bfa37635645d68fead22795
W. Brian Gourlie <bgourlie@gmail.com>
2014-12-29 21:09:54 -0600

    Add initial UI for recording stuff.

--- client/lib/src/components/entry_screen/entry_screen.dart
+++ client/lib/src/components/entry_screen/entry_screen.dart
@@ -3,6 +3,7 @@ library entry_screen;
 import 'package:angular/angular.dart';
 import 'package:di/annotations.dart';
 import 'package:logging/logging.dart';
+import 'package:client/fitlog_models.dart';
 
 @Injectable()
 @Component(
@@ -11,4 +12,15 @@ import 'package:logging/logging.dart';
     cssUrl: 'packages/client/src/components/entry_screen/entry_screen.css')
 class EntryScreen {
   static final _logger = new Logger('nobs_entry_screen');
+  Exercise selectedExercise;
+
+  void onExerciseSelected(Exercise e){
+    _logger.finest('${e.title} selected');
+    selectedExercise = e;
+  }
+
+  void cancelRecord(){
+    _logger.finest('cancel record called.');
+    selectedExercise = null;
+  }
 }
--- client/lib/src/components/find_widget/find_widget.dart
+++ client/lib/src/components/find_widget/find_widget.dart
@@ -1,34 +1,58 @@
 library find_widget;
 
+import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:di/annotations.dart';
 import 'package:logging/logging.dart';
+import 'package:rate_limit/rate_limit.dart';
 import 'package:client/fitlog_models.dart';
 import 'package:client/src/services/find_engine/find_engine.dart';
 import 'package:client/src/services/exercise_find_engine.dart';
 
+typedef SelectedHandler(Exercise exercise);
+
 @Injectable()
 @Component(selector: 'find-widget',
     templateUrl: 'packages/client/src/components/find_widget/find_widget.html',
-    cssUrl: 'packages/client/src/components/find_widget/find_widget.css')
-class FindWidget {
-    static final _logger = new Logger('find_widget');
-    final ExerciseFindEngine _findEngine;
-    final findResults = new List<FindResult<Exercise>>();
-    String searchTerm;
+    cssUrl: 'packages/client/src/components/find_widget/find_widget.css',
+    map: const {
+      'selected-handler' : '&selectedHandler'
+    })
+class FindWidget implements ShadowRootAware {
+  static final _logger = new Logger('find_widget');
+  final ExerciseFindEngine _findEngine;
+  final findResults = new List<FindResult<Exercise>>();
+  Function selectedHandler;
+  String searchTerm;
 
-    FindWidget(this._findEngine);
+  FindWidget(this._findEngine);
 
-    void onFindRequest(String term, int onTermType){
-        this.searchTerm = term;
-        if(this.searchTerm.length == 0){
-            findResults.clear();
-        }else{
-            _logger.finest('Handling find request for "$term"');
-            findResults.clear();
-            _findEngine.streamResults(term).listen((FindResult<Exercise> e){
-                findResults.add(e);
-            });
-        }
+  void onShadowRoot(ShadowRoot shadowRoot){
+    shadowRoot.querySelector('#term').onKeyDown.transform(
+        new Debouncer(const Duration(milliseconds: 250))).forEach(
+            (e) => doSearch());
+  }
+
+  void doSearch(){
+    if(this.searchTerm.length == 0){
+      findResults.clear();
+    }else{
+      _logger.finest('Handling find request for "$searchTerm"');
+      findResults.clear();
+      _findEngine.streamResults(searchTerm)
+          .listen((FindResult<Exercise> e){
+            findResults.add(e);
+          });
+      }
+  }
+
+  void handleSelect(Exercise e){
+    if(selectedHandler != null){
+      SelectedHandler handler = selectedHandler();
+      handler(e);
     }
+
+    findResults.clear();
+    searchTerm = null;
+  }
 }

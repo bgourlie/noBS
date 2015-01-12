bb2add211f4e31c2335532c2a0fee04fea1ce1ba
W. Brian Gourlie <bgourlie@gmail.com>
2015-01-12 13:02:04 -0600

    Disable server tests for now.

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -8,10 +8,10 @@ ls
 pub get
 pub run grinder:grinder tests
 
-echo "Running Server Tests..."
-cd ../server
-kpm restore -s https://www.myget.org/F/aspnetvnext/api/v2/ -f https://nuget.org/api/v2/ --quiet
-
-cd ./test/FitLog.Domain.Tests/
-
-k test
+#echo "Running Server Tests..."
+#cd ../server
+#kpm restore -s https://www.myget.org/F/aspnetvnext/api/v2/ -f https://nuget.org/api/v2/ --quiet
+#
+#cd ./test/FitLog.Domain.Tests/
+#
+#k test

94944e1b2faa886f6287ef3455097cdf7e3d7e1a
W. Brian Gourlie <bgourlie@gmail.com>
2015-01-14 11:27:32 -0600

    Don't deploy server files for the time being.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -27,8 +27,8 @@ then
 	echo "==========================="
 	echo "      DEPLOY TO AZURE      "
 	echo "==========================="
-	cd $TRAVIS_BUILD_DIR/server/src/FitLog.Api
-	find ./deploy -type f -exec curl --fail --ftp-create-dirs -T {} -u $AZURE_FTP_USER:$AZURE_FTP_PASSWORD ftp://waws-prod-dm1-003.ftp.azurewebsites.windows.net/site/wwwroot/nobs-api/{} \;
+	cd ~
+	find $DEPLOY_DIR -type f -exec curl --fail --ftp-create-dirs -T {} -u $AZURE_FTP_USER:$AZURE_FTP_PASSWORD ftp://waws-prod-dm1-003.ftp.azurewebsites.windows.net/site/wwwroot/nobs-api/{} \;
 else
 	echo "Skipping server deployment because branch is not $DEPLOY_BRANCH."	
 	exit 0
--- ci/pack-deployment.sh
+++ ci/pack-deployment.sh
@@ -26,30 +26,32 @@ echo "==========================="
 echo "    PACKING DEPLOYMENT     "
 echo "==========================="
 
+mkdir -p $DEPLOY_DIR/wwwroot
+
 echo "Building client..."
-cd ./client
+cd $TRAVIS_BUILD_DIR/client
 pub build
 
-# (HACK) Move CLR KRE to packages folder
-# Hopefully we will have the ability to target azure deployments from OS X/Linux at some point
-cd $TRAVIS_BUILD_DIR/server/src
-cp -rf ./AzureDeployDeps/KRE-CLR-x86.1.0.0-beta1 ~/.kre/packages/KRE-CLR-x86.1.0.0-beta1
+## (HACK) Move CLR KRE to packages folder
+## Hopefully we will have the ability to target azure deployments from OS X/Linux at some point
+#cd $TRAVIS_BUILD_DIR/server/src
+#cp -rf ./AzureDeployDeps/KRE-CLR-x86.1.0.0-beta1 ~/.kre/packages/KRE-CLR-x86.1.0.0-beta1
+#
+#echo "Packing server..."
+#cd ./FitLog.Api
+#kpm pack --out ./deploy --no-source --runtime ~/.kre/packages/KRE-CLR-x86.1.0.0-beta1
+#
+## Generate the version file
+#pub global activate -sgit https://github.com/bgourlie/nobsVersionPatcher
+#pub global run nvp:nvp genServerVersion -o ./deploy/wwwroot/version.json
+#
+## (HACK) Copy additional dependencies not packed on OS X / linux for deployment to azure
+#cp -rf ../AzureDeployDeps/System.ComponentModel ./deploy/approot/packages/System.ComponentModel
+#cp -rf ../AzureDeployDeps/System.ComponentModel.Annotations ./deploy/approot/packages/System.ComponentModel.Annotations
+#
+#NUM_SERVER_FILES=$(find ./deploy -type f | wc -l)
+#echo "Packed server files ($NUM_SERVER_FILES files)"
 
-echo "Packing server..."
-cd ./FitLog.Api
-kpm pack --out ./deploy --no-source --runtime ~/.kre/packages/KRE-CLR-x86.1.0.0-beta1
-
-# Generate the version file
-pub global activate -sgit https://github.com/bgourlie/nobsVersionPatcher
-pub global run nvp:nvp genServerVersion -o ./deploy/wwwroot/version.json
-
-# (HACK) Copy additional dependencies not packed on OS X / linux for deployment to azure
-cp -rf ../AzureDeployDeps/System.ComponentModel ./deploy/approot/packages/System.ComponentModel
-cp -rf ../AzureDeployDeps/System.ComponentModel.Annotations ./deploy/approot/packages/System.ComponentModel.Annotations
-
-NUM_SERVER_FILES=$(find ./deploy -type f | wc -l)
-echo "Packed server files ($NUM_SERVER_FILES files)"
-
-NUM_CLIENT_FILES=$(find ../../../client/build -type f | wc -l)
+NUM_CLIENT_FILES=$(find $TRAVIS_BUILD_DIR/client/build -type f | wc -l)
 echo "Copying client files to deployment ($NUM_CLIENT_FILES files)"
-cp -rf ../../../client/build/web/* ./deploy/wwwroot
+cp -rf $TRAVIS_BUILD_DIR/client/build/web/* $DEPLOY_DIR/wwwroot
